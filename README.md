e61c85f3ffacc6d525e4b4f8fb3952b9d53d78da
himansh <himansh@atomicmail.io>
2026-05-02 17:14:06 +0530

    Launched with a screen

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # calculator
 A simple calculator.
+
+## attribution
+Icon Image: Under CC0 License, <a href="https://www.svgrepo.com/svg/288223/calculator" target="_blank">view here </a>
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use fltk::{
+    app::{self, Scheme},
+    frame::Frame,
+    image::PngImage,
+    prelude::*,
+    window::Window,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let app = app::App::default().with_scheme(Scheme::Gtk);
+    let mut wind = Window::new(100, 100, 400, 450, "Calculator");
+
+    let icon = PngImage::from_data(include_bytes!("icon.png")).unwrap();
+    wind.set_icon(Some(icon));
+
+    let mut frame = Frame::new(0, 0, 400, 150, "Yokuso!");
+    wind.add(&frame);
+
+    wind.end();
+    wind.show();
+    app.run().unwrap();
 }
